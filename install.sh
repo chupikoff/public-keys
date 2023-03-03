0ea0e798fef666203d5ccbf365ace342fe6c3d2a
chupikoff <54735023+chupikoff@users.noreply.github.com>
2023-03-03 20:47:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-useradd hes
+if ! id -u hes > /dev/null 2>&1; then
+  useradd hes
+fi
 usermod -aG wheel hes
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 echo 'hes        ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
 mkdir /home/hes/.ssh
-wget -P /home/hes/.ssh https://raw.githubusercontent.com/chupikoff/install/main/authorized_keys
+if ! [ -f /home/hes/.ssh/authorized_keys ]; then
+  wget -P /home/hes/.ssh https://raw.githubusercontent.com/chupikoff/install/main/authorized_keys
+fi
 chown -R hes. /home/hes
 chmod 0700 /home/hes/.ssh
 chmod 0600 /home/hes/.ssh/authorized_keys
 sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sed -i '#ListenAddress 0.0.0.0/ListenAddress 88.85.89.70/' /etc/ssh/sshd_config
-sed -i '#PermitRootLogin yes/PermitRootLogin no' /etc/ssh/sshd_config
+sed -i '/#ListenAddress 0.0.0.0/ListenAddress 88.85.89.70/' /etc/ssh/sshd_config
+sed -i '/#PermitRootLogin yes/PermitRootLogin no' /etc/ssh/sshd_config
 echo "alias s='sudo su -'" >> /home/hes/.bashrc
 timedatectl set-timezone Europe/Amsterdam
 reboot
